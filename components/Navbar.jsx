e851833952e524124108f24e1543e4cdfda89918
Devsi <grace.emily876@gmail.com>
2024-03-13 14:16:59 +0530

    changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import Link from "next/link";
 
+const REACT_APP_GATEWAY_URL = "https://gateway.netsepio.com/";
+
 const Navbar = () => {
   const wallet = Cookies.get("tarot_wallet");
 
@@ -23,37 +25,76 @@ const Navbar = () => {
   };
 
   const connectWallet = async () => {
-   
+    const wallet = getAptosWallet();
     try {
+      const response = await wallet.connect();
+
+      const account = await wallet.account();
+      console.log("account", account);
+
       const networkwallet = await window.aptos.network();
 
       // Check if the connected network is Mainnet
       if (networkwallet === "Testnet") {
+        const { data } = await axios.get(
+          `${REACT_APP_GATEWAY_URL}api/v1.0/flowid?walletAddress=${account.address}`
+        );
+        console.log(data);
 
-        const wallet = getAptosWallet();
-        const response = await wallet.connect();
+        const message = data.payload.eula;
+        const nonce = data.payload.flowId;
+        const publicKey = account.publicKey;
 
-        const account = await wallet.account();
-        console.log("account", account);
-        Cookies.set("tarot_wallet", account.address, { expires: 7 });
-        window.location.reload();
+        const { signature, fullMessage } = await wallet.signMessage({
+          message,
+          nonce,
+        });
+        console.log("sign", signature, "full message", fullMessage);
+
+        const authenticationData = {
+          flowId: nonce,
+          signature: `0x${signature}`,
+          pubKey: publicKey,
+        };
+
+        const authenticateApiUrl = `${REACT_APP_GATEWAY_URL}api/v1.0/authenticate`;
+
+        const config = {
+          url: authenticateApiUrl,
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          data: authenticationData,
+        };
+
+        try {
+          const response = await axios(config);
+          console.log("auth data", response.data);
+          const token = await response?.data?.payload?.token;
+          const userId = await response?.data?.payload?.userId;
+          // localStorage.setItem("platform_token", token);
+          Cookies.set("tarot_token", token, { expires: 7 });
+          Cookies.set("tarot_wallet", account.address, { expires: 7 });
+          Cookies.set("tarot_userid", userId, { expires: 7 });
+
+          window.location.reload();
+        } catch (error) {
+          console.error(error);
+        }
       } else {
         alert(`Switch to Testnet in your wallet`);
       }
     } catch (err) {
-      console.log(err, err.message);
+      console.log(err);
     }
   };
 
   const handleDeleteCookie = () => {
     Cookies.remove("tarot_wallet");
+    Cookies.remove("tarot_token");
     window.location.href = "/";
   };
-
-  const disconnect = async () => {
-    const wallet = getAptosWallet();
-    await wallet.disconnect();
-  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -95,7 +136,6 @@ const Navbar = () => {
       ) : (
         <>
         <button onClick={connectWallet}>Connect wallet</button>
-        <button onClick={disconnect}>disconnect</button>
         </>
       )}
     </div>
